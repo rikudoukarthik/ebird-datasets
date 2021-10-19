9ea90fbde15495883b6c33a6f6c7969ddb2048d1
Karthik Thrikkadeeri <rikudoukarthik@gmail.com>
2021-10-19 14:30:10 +0530

    September monthly challenge

--- ebd_IN_relSep-2021.R
+++ ebd_IN_relSep-2021.R
@@ -1,22 +1,21 @@
-require(lubridate)
-require(tidyverse)
+library(lubridate)
+library(tidyverse)
 
 preimp <- c("CATEGORY","COMMON.NAME","OBSERVATION.COUNT",
             "LOCALITY.ID","LOCALITY.TYPE","REVIEWED","APPROVED","STATE","COUNTY","LAST.EDITED.DATE",
             "LATITUDE","LONGITUDE","OBSERVATION.DATE","TIME.OBSERVATIONS.STARTED","OBSERVER.ID",
             "PROTOCOL.TYPE","DURATION.MINUTES","EFFORT.DISTANCE.KM","LOCALITY","BREEDING.CODE",
             "NUMBER.OBSERVERS","ALL.SPECIES.REPORTED","GROUP.IDENTIFIER","SAMPLING.EVENT.IDENTIFIER",
-            "TRIP.COMMENTS")
+            "TRIP.COMMENTS","HAS.MEDIA")
 
 
-rawpath <- "ebd_IN_relAug-2021.txt"
+rawpath <- "ebd_IN_relSep-2021.txt"
 nms <- names(read.delim(rawpath, nrows = 1, sep = "\t", header = T, quote = "", stringsAsFactors = F,
                         na.strings = c(""," ", NA)))
 nms[!(nms %in% preimp)] <- "NULL"
 nms[nms %in% preimp] <- NA
-# 5 mins
 data <- read.delim(rawpath, colClasses = nms, sep = "\t", header = T, quote = "",
-                      stringsAsFactors = F, na.strings = c(""," ",NA))
+                   stringsAsFactors = F, na.strings = c(""," ",NA)) # 7 mins
 
 
 ### sensitive species
@@ -29,7 +28,18 @@ senssp <- read.delim(senspath, colClasses = nms1, sep = "\t", header = T, quote 
                      stringsAsFactors = F, na.strings = c(""," ",NA))
 
 
-data <- rbind(data, senssp)
+data <- rbind(data, senssp) # <1min
+
+
+
+met_week <- function(dates) {
+  require(lubridate)
+  normal_year <- c((0:363 %/% 7 + 1), 52)
+  leap_year   <- c(normal_year[1:59], 9, normal_year[60:365])
+  year_day    <- yday(dates)
+  return(ifelse(leap_year(dates), leap_year[year_day], normal_year[year_day])) 
+}
+
 
 
 ### adding useful columns (4 mins)
@@ -41,15 +51,14 @@ data <- data %>%
          MONTH = month(OBSERVATION.DATE),
          DAYM = day(OBSERVATION.DATE),
          DAYY = yday(OBSERVATION.DATE),
-         # WEEKY = met_week(OBSERVATION.DATE),
-         # SYEAR = if_else(DAYY <= 151, YEAR-1, YEAR), # SY = 1st June to 31st May
-         # WEEKSY = if_else(WEEKY > 21, WEEKY-21, 52-(21-WEEKY))
-         )
+         WEEKY = met_week(OBSERVATION.DATE),
+         SYEAR = if_else(DAYY <= 151, YEAR-1, YEAR), # SY = 1st June to 31st May
+         WEEKSY = if_else(WEEKY > 21, WEEKY-21, 52-(21-WEEKY)))
 
 rm(list=setdiff(ls(envir = .GlobalEnv), c("data")), pos = ".GlobalEnv")
-save.image("ebd_IN_relAug-2021.RData") # 4.5 mins
+save.image("ebd_IN_relSep-2021.RData") # 7 mins
 
 
-### Month of August for monthly challenge
-data <- data %>% filter(YEAR == 2021 & MONTH == 8)
-save.image("ebd_IN_relAug-2021_AUG.RData")
+### Month of September for monthly challenge
+data <- data %>% filter(YEAR == 2021 & MONTH == 9)
+save.image("ebd_IN_relSep-2021_SEP.RData")
