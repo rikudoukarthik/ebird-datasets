4288a9c60b51ee802eea7cff7d5c7d724fc993fd
rikudoukarthik <rikudoukarthik@gmail.com>
2022-01-31 22:41:53 +0530

    sound ID candidate list + added .gitattribute

--- sound-id-vol/sound-id-vol.R
+++ sound-id-vol/sound-id-vol.R
@@ -3,7 +3,7 @@ library(lubridate)
 library(magrittr)
 
 media_csv_names <- list.files(path = "sound-id-vol/",
-                              pattern = "*.csv",
+                              pattern = "ML_*",
                               full.names = T)
 
 eBird.users <- read.delim("EBD/ebd_users_relDec-2021.txt", sep = "\t", header = T, quote = "", 
@@ -21,15 +21,26 @@ data0 <- media_csv_names %>%
   rename_with(~ gsub(" ", ".", .x)) %>% 
   left_join(eBird.users)
 
+# getting most active state (audio-wise) for each observer
+temp <- data0 %>% 
+  group_by(FULL.NAME) %>% 
+  count(STATE) %>% 
+  arrange(desc(n)) %>% 
+  slice(1) %>% 
+  transmute(FULL.NAME = FULL.NAME,
+            ACTIVE.STATE = STATE)
+
 data1 <- data0 %>% 
   group_by(FULL.NAME) %>% 
-  summarise(TOT.AUDIO = n(),
+  summarise(TOT.AUDIO = n(), # each row in the data is one recording
             SP.AUDIO = n_distinct(COMMON.NAME)) %>% 
-  arrange(desc(TOT.AUDIO), desc(SP.AUDIO))
+  left_join(temp) %>% 
+  arrange(ACTIVE.STATE, desc(TOT.AUDIO), desc(SP.AUDIO))
 
+# setting 150 species and 400 recordings as threshold
 data2 <- data1 %>% 
-  filter(TOT.AUDIO >= 500, SP.AUDIO >= 200)
+  filter(TOT.AUDIO >= 400, SP.AUDIO >= 150)
 
-data3 <- data1 %>% 
-  filter(TOT.AUDIO >= 200, SP.AUDIO >= 100)
+write_csv(data2, "sound-id-vol/sound-id-vol_candidate-list.csv")
+
 
